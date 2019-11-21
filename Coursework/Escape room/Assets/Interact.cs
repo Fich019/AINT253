d64f274a5cce9cbbb30785bdbeb15ac9d59ed878
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-11-21 23:41:29 +0000

    Tweaks and crosshair scaling

---
+++ Coursework/Escape room/Assets/Interact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField] Sprite image;
+    [SerializeField] float iwidth;
+    [SerializeField] float iheight;
+    private RectTransform rt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.transform.tag == "Player"){
+            rt.sizeDelta = new Vector2(image.rect.width * iwidth, image.rect.height * iheight);
+        }
+    }
+}
--- Coursework/Escape room/Assets/interactRay.cs
+++ Coursework/Escape room/Assets/interactRay.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class interactRay : MonoBehaviour
 {
+    
+    [SerializeField] float RayDistance;
+    [SerializeField] float scale;
+    [SerializeField] Image image;
+    private float maxSize = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,34 @@ public class interactRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //RaycastHit TestRay;
+        //if (Physics.Raycast(Camera.position, transform.TransformDirection(Camera.forward), out TestRay, RayDistance))
+        //{
+        //    Debug.DrawRay(Camera.position, transform.TransformDirection(Camera.forward) * TestRay.distance, Color.yellow);
+
+        //}
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(transform.position, transform.forward * 7, Color.yellow);
+
+        if (Physics.Raycast(ray, out hit, RayDistance))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            Debug.DrawRay(transform.position, transform.forward * 7, Color.yellow);
+            if (hit.transform.name == "Cube")
+            {
+                if (image.transform.localScale.x < maxSize)
+                {
+                    image.transform.localScale = image.transform.localScale * scale;
+                }
+                
+            }
+        }
+
+        if (image.transform.localScale.x >= -maxSize)
+        {
+            image.transform.localScale = image.transform.localScale / scale;
+        }
     }
 }
